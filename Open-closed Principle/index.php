16ae63a0d2bb7c56048a03ad7d096add5c32f20b
Rubel <mofrubel@gmail.com>
2017-02-07 02:40:45 +0600

    Initial commit with complete Single Repository and Open-Close Principle

---
+++ Open-closed Principle/index.php
@@ -0,0 +1,82 @@
+<?php
+
+/*
+** Open for Extension and Close for Modification
+** One Class should be extended rather than modifying it, 
+** Objects or entities should be open for extension, but closed for modification
+** Here Sum method of AreaCalculator Class should be extended rather than modifing it for another Area of different Shaped Class 
+*/
+
+Class Circle{
+	public $radius;
+
+	public function __construct($radius) {
+		$this->radius = $radius;
+	}
+
+	public function area() {
+		return 3.1416 * pow($this->radius, 2);
+	}
+}
+
+Class Square{
+	public $length;
+
+	public function __construct($length) {
+		$this->length = $length;
+	}
+
+	public function area() {
+		return pow($this->length, 2);
+	}
+}
+
+Class Triangle{
+	public $base;
+	public $height;
+
+	public function __construct($base, $height) {
+		$this->base = $base;
+		$this->height = $height;
+	}
+
+	public function area() {
+		return 0.5 * $this->base * $this->height;
+	}
+}
+
+Class AreaCalculator{
+	protected $shapes;
+
+	public function __construct($shapes = array()) {
+		$this->shapes= $shapes;
+	}
+
+	public function sum() {
+		foreach($this->shapes as $shape) {
+	        $area[] = $shape->area();
+	    }
+
+	    return array_sum($area);
+	}
+
+	public function output() {
+		return implode("", array(
+			"<h1>", 
+				"Sum of the areas of provided shapes: ",
+				$this->sum(),
+            "</h1>"
+			));
+	}
+}
+
+$shapes = array(
+    new Circle(2),
+    new Square(5),
+    new Square(6),
+    new Triangle(4,5)
+);
+
+$areas = new AreaCalculator($shapes);
+
+echo $areas->output();
---
+++ Single Repository/index.php
@@ -0,0 +1,62 @@
+<?php
+/*
+** Single Repository Example
+** One Class will serve only one purpose
+** A class should have one and only one reason to change, meaning that a class should have only one job
+** Here Square, Circle and AreaCalculator Class serve their own purposes
+*/
+
+Class Circle{
+	public $radius;
+
+	public function __construct($radius) {
+		$this->radius = $radius;
+	}
+}
+
+Class Square{
+	public $length;
+
+	public function __construct($length) {
+		$this->length = $length;
+	}
+}
+
+Class AreaCalculator{
+	protected $shapes;
+
+	public function __construct($shapes = array()) {
+		$this->shapes= $shapes;
+	}
+
+	public function sum() {
+		foreach($this->shapes as $shape) {
+	        if(is_a($shape, 'Square')) {
+	            $area[] = pow($shape->length, 2);
+	        } else if(is_a($shape, 'Circle')) {
+	            $area[] = pi() * pow($shape->radius, 2);
+	        }
+	    }
+
+	    return array_sum($area);
+	}
+
+	public function output() {
+		return implode("", array(
+			"<h1>", 
+				"Sum of the areas of provided shapes: ",
+				$this->sum(),
+            "</h1>"
+			));
+	}
+}
+
+$shapes = array(
+    new Circle(2),
+    new Square(5),
+    new Square(6)
+);
+
+$areas = new AreaCalculator($shapes);
+
+echo $areas->output();
